dcc8f951f2ce06246bd347f6c38105915e179a44
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2024-11-08 13:13:33 +0000

    Destructuring basics

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,56 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27]
+// non-ES6 way:
+// let john = ages[0]
+// let mary = ages[1]
+// let joe = ages[2]
+// ES6 destructuring way:
+let [john, mary, joe] = ages;
+console.log(john, mary, joe)
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
 
 // Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+//use commas to skip values you dont want
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+//even easier with objects
+let languages2 = {
+    first: "eng",
+    second: "fra",
+    third: "ger",
+    fourth: "jap",
+};
+let {first, third} = languages2;
+console.log(first, third);
 
 // Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite)
+console.log(secondFavourite)
+console.log(others)
+
+let favouriteFoods = {
+    brian: "pasta",
+    anna: "pizza",
+    sarah: "veg",
+    andrea: "steak"
+};
+let {brian, anna, ...rest } = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
